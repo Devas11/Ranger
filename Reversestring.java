860c3fecd57ca8ffc938a4e52a70e378888ec205
Devas11 <devakpr96@gmail.com>
2017-06-14 18:26:56 +0530

    Create Reversestring.java

--- Reversestring.java
+++ Reversestring.java
@@ -1,19 +1,19 @@
 import java.util.*;
  
-class ReverseString
+class Reversestring
 {
    public static void main(String args[])
    {
-      String original, reverse = "";
-      Scanner in = new Scanner(System.in);
+      String  reverse = "";
+      Scanner s = new Scanner(System.in);
  
       System.out.println("Enter a string to reverse");
-      original = in.nextLine();
+      String a = s.nextLine();
  
-      int length = original.length();
+      int length = a.length();
  
       for ( int i = length - 1 ; i >= 0 ; i-- )
-         reverse = reverse + original.charAt(i);
+         reverse = reverse + a.charAt(i);
  
       System.out.println("Reverse of entered string is: "+reverse);
    }
